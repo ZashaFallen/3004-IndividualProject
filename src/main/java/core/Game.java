7c35694cd35941809dd244a12737e5dec8671cdc
Elijah MacPherson <elijah.macpherson@gmail.com>
2018-09-12 11:25:08 -0400

    main: Added the Game class, where the primary game loop will be.

---
+++ src/main/java/core/Game.java
@@ -0,0 +1,12 @@
+package core;
+
+public class Game {
+	
+	private static final String[] validInputs =  {"C", "F", "H", "S"};
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+}
