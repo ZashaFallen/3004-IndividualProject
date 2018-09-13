4f54049ae6f4b1180ee2c6db610f1059fc362367
Elijah MacPherson <elijah.macpherson@gmail.com>
2018-09-13 16:36:20 -0400

    test: created additional Hand.getScore() tests for testing face cards and aces.

--- src/test/java/core/HandClassTests.java
+++ src/test/java/core/HandClassTests.java
@@ -18,18 +18,51 @@ public class HandClassTests extends TestCase{
 	public void testGetScoreAces() {
 		Hand hand = new Hand();
 		
+		// Ace can count as 1
 		hand.add(new Card("C", "K", 10));
 		hand.add(new Card("D", "5", 5));
 		assertEquals(15, hand.getScore());
-		hand.add(new Card("A", "A", 0));
+		hand.add(new Card("S", "A", 0));
 		assertEquals(16, hand.getScore());
 		
+		// Ace can count as 11
 		hand = new Hand();
-		hand.add(new Card("A", "A", 0));
-		hand.add(new Card("A", "A", 0));
-		assertEquals(12, hand.getScore());
-		hand.add(new Card("9", "9", 9));
+		hand.add(new Card("D", "K", 10));
+		hand.add(new Card("C", "A", 0));
 		assertEquals(21, hand.getScore());
+		
+		// A hand can count two Aces worth 1 each
+		hand = new Hand();
+		hand.add(new Card("D", "K", 10));
+		hand.add(new Card("C", "9", 9));
+		assertEquals(19, hand.getScore());
+		hand.add(new Card("H", "A", 0));
+		hand.add(new Card("D", "A", 0));
+		assertEquals(21, hand.getScore());
+		
+		// Ace can count as 11 then 1
+		hand = new Hand();
+		hand.add(new Card("C", "A", 0));
+		hand.add(new Card("H", "8", 8));
+		assertEquals(19, hand.getScore());
+		hand.add(new Card("H", "9", 9));
+		assertEquals(18, hand.getScore());
+	}
+	
+	@Test
+	public void testGetScoreFaceCards() {
+		Hand hand = new Hand();
+		
+		hand.add(new Card("H", "J", 10));
+		assertEquals(10, hand.getScore());
+		
+		hand = new Hand();
+		hand.add(new Card("H", "Q", 10));
+		assertEquals(10, hand.getScore());
+		
+		hand = new Hand();
+		hand.add(new Card("H", "K", 10));
+		assertEquals(10, hand.getScore());
 	}
 	
 }
