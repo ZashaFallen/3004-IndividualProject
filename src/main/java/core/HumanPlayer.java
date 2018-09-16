61e5db53cbd893de12f656e557d9952780fc865a
ElijahMacPherson <elijahmacpherson@cmail.carleton.ca>
2018-09-15 20:49:31 -0400

    main: completed implementing split capability for the human player

--- src/main/java/core/HumanPlayer.java
+++ src/main/java/core/HumanPlayer.java
@@ -8,9 +8,13 @@ public class HumanPlayer extends Player {
 	
 	@Override
 	public String showHand() {
-		String handString = "Your Hand: " + hand.getCards();
-		if (splitHand != null) {
-			handString += "\r\nYour Second Hand: " + splitHand.getCards();
+		String handString; 
+		if (splitHand == null) {
+			handString = "Your Hand: " + hand.getCards();
+		}
+		else {
+			handString = "Your First Hand: " + hand.getCards() + 
+						 "\r\nYour Second Hand: " + splitHand.getCards();
 		}
 		return handString;
 	}
@@ -20,19 +24,21 @@ public class HumanPlayer extends Player {
 		String response = "";
 		String extraHandText = "";
 		
-		if (hand.canSplit()) {
-			extraHandText = " for your first hand";
-		}
-		
 		while (!response.equals("S") && getBestHandState() != PlayerState.busted) {
-			response = ConsoleIO.input("\r\nWould you like to (h)hit, (s)stay, or (d)split?: ");
+			response = ConsoleIO.input("\r\nWould you like to (h)hit, (s)stay, or (d)split? " + extraHandText + ": ");
 			
 			if (response.equals("H")) {
 				System.out.println("You hit" + extraHandText + ": " + currentHand.hit(deck).toString());
 				System.out.println("Current score" + extraHandText + ": " + currentHand.getScore());
 				
-				if (getBestHandState() == PlayerState.busted) {
+				if(currentHand.getState() == PlayerState.busted) {
 					System.out.println("\r\nYou bust" + extraHandText + "!");
+					
+					if (currentHand == hand && split) {
+						currentHand = splitHand;
+						response = "";
+						extraHandText = " on your second hand";
+					}
 				}
 			}
 			else if (response.equals("S")) {
@@ -47,7 +53,8 @@ public class HumanPlayer extends Player {
 				if (hand.canSplit() && !split) {
 					System.out.println("You split!");
 					split(deck);
-					showHand();
+					extraHandText = " for your first hand";
+					System.out.println(showHand());
 				}
 				else {
 					System.out.println("You may only split when your initial two cards are of the same rank.\r\n\r\n");
